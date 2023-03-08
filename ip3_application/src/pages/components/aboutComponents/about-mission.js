b22e5f26317010c370c86d93b0174eb2b75f723c
AretiZ <74565922+AZILA200@users.noreply.github.com>
2023-03-08 14:59:40 +0000

    Update about-mission.js

--- ip3_application/src/pages/components/aboutComponents/about-mission.js
+++ ip3_application/src/pages/components/aboutComponents/about-mission.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const AboutMission = () => {
     return (
+        
         <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-6">
-            <h1>Our Mission</h1>
-            <p>Some Information About Our Mission.</p>
+            <div class="col-sm-2 "> </div>
+            <div class="col-sm-6 mt-4">
+            <h className="hdr">Our Mission</h>
+            <div class="border border border-dark ">
+            <p className='text'>Some Information About Our Mission.</p> 
+            </di>
             </div>
         </div>
+       
     )
 }
 
