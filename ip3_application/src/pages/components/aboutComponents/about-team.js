459f81d99575c2bd7c48066963af8f76f9443f41
AretiZ <74565922+AZILA200@users.noreply.github.com>
2023-03-08 15:00:52 +0000

    Update about-team.js

--- ip3_application/src/pages/components/aboutComponents/about-team.js
+++ ip3_application/src/pages/components/aboutComponents/about-team.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
+
 const AboutTeam = () => {
     return (
-        <div class="row float-end">
-            <div class="col-sm-8">
-                <h1>Our Team</h1>
-                <p>Some Information About Our Crack Team of Comp Sci Students.</p>
-            </div>
-            <div class="col-sm-2"></div>
-        </div>
+      <div class="row">
+      <div class="col-sm-2 "></div>
+      <div class="col-sm-6 mt-4">
+      <h className="hdr">Our Team</h>
+      <div class="border border border-dark ">
+      <p className='text'>Some Information About Our Team.</p> 
+      </div>
+      
+      </div>
+      
+      </div>
+           
+           
+       
     )
 }
 
-export default AboutTeam;
+export default AboutTeam
