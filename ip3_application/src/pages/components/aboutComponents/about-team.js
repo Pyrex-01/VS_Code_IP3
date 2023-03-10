a5d2cd722f031d94fa29f81b8eebf4b070da7005
Polina Gavrilova <polina18102001@outlook.com>
2023-03-10 11:38:16 +0000

    update

--- ip3_application/src/pages/components/aboutComponents/about-team.js
+++ ip3_application/src/pages/components/aboutComponents/about-team.js
@@ -2,22 +2,24 @@ import React from 'react';
 
 
 const AboutTeam = () => {
-    return (
-      <div class="row">
-      <div class="col-sm-2 "></div>
+  return (
+    <div class="row">
+      <div class="col-sm-2 ">
+        
+      </div>
       <div class="col-sm-6 mt-4">
-      <h className="hdr">Our Team</h>
-      <div class="border border border-dark ">
-      <p className='text'>Some Information About Our Team.</p> 
+        <h className="hdr">Our Team</h>
+        <div class="border border border-dark ">
+          <p className='text'>Some Information About Our Team.</p>
+        </div>
+
       </div>
-      
-      </div>
-      
-      </div>
-           
-           
-       
-    )
+
+    </div>
+
+
+
+  )
 }
 
 export default AboutTeam
