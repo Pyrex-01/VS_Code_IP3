d70bbb81ba643f6c1ffb729088bed3f6d224e8aa
AretiZ <74565922+AZILA200@users.noreply.github.com>
2023-03-19 14:39:29 +0000

    Update About.js

--- ip3_application/src/pages/About.js
+++ ip3_application/src/pages/About.js
@@ -1,68 +1,38 @@
-import React from "react";
+import React from 'react';
+import AboutMission from './components/aboutComponents/about-mission';
+import AboutTeam from './components/aboutComponents/about-team';
 
-const About= () => {
+function About() {
+    const [data, setData] = React.useState(null);
 
-        return (
-            <div className="screen">
+    React.useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setData(data.message));
+    }, []);
+
+    return (
+        <div className="screen">
                 <div className='header'>
                     <div className="about">
                     <h className="h_about">About us</h>
                     </div>
                 </div>
-
-            < div className="left-card">
-                 <div class="row">
                 
-                    <div className="col-md-4"> 
-                        <div className="card-header">
-                        <h class="card-title">Our Mission</h>
-                        </div>
-                        <div className="card-body">
-                        <p class="card-text border ">The Mission of the project is to enable users from around the world to track planes that are shown on a real-time map. 
-                                             Additionally,noffer the users the ability to view information like plane details, weather, the estimation of planes' CO2 emissions, 
-                                             and suggestions of new locations that aviation enthusiasts can explore and exchange their experienses through our blog page . 
-                        </p>
-                        </div>
-                        
-                        <div className="col-md-2">
-                        <img></img>
-                    </div>
-                    </div> 
-                 </div>
-            </div>
+            <p></p>
 
-            
+            <AboutMission />
+            <br></br>
+            <AboutTeam />
+            <br></br>
+            <p>{!data ? "Loading..." : data}</p>
 
-            < div className="right-card">
-                 <div class="row">
-
-                <div className="col-md-2">
-
-                    </div>
-                    <div className="col-md-4"> 
-                        <div className="card-header">
-                        <h class="card-title_2">Our Team</h>
-                        </div>
-                        <div className="card-body">
-                        <p class="card-text_2 border">Info about the team
-                        </p>
-                        </div>
-                        
-                        
-                    </div> 
-                 </div>
-            </div>
-
-
-      <div className="Footer_1">
+            <div className="Footer_1">
         <p className="foot ">This project was created by students of Glasgow Caledonian University</p>
         <p>2023</p>
-      </div>
-              
-</div>
-            
-
-        )
+        </div>
+        </div>
+    )
 }
 
 export default About;
