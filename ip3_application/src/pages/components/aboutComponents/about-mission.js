ce86e4f4aaa1a19f46c5fe038f9952f88fba8140
AretiZ <74565922+AZILA200@users.noreply.github.com>
2023-03-19 14:55:03 +0000

    Update about-mission.js

--- ip3_application/src/pages/components/aboutComponents/about-mission.js
+++ ip3_application/src/pages/components/aboutComponents/about-mission.js
@@ -6,7 +6,7 @@ const AboutMission = () => {
         <div class="row">
             <div class="col-sm-2 "> </div>
             <div class="col-sm-6 mt-4">
-                <h className="hdr">Our Mission</h>
+                <h className="h_about">Our Mission</h>
                 <div class="border border border-dark ">
                     <p className='text'>Some Information About Our Mission.</p>
                 </div>
