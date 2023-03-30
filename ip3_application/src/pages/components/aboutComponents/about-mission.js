e9064bbefba1f57145bdc4980770d5dc04219da0
AretiZ <74565922+AZILA200@users.noreply.github.com>
2023-03-30 18:37:42 +0100

    Update about-mission.js

--- ip3_application/src/pages/components/aboutComponents/about-mission.js
+++ ip3_application/src/pages/components/aboutComponents/about-mission.js
@@ -8,7 +8,10 @@ const AboutMission = () => {
             <div class="col-sm-6 mt-4">
                 <h className="h_about">Our Mission</h>
                 <div class="border border border-dark ">
-                    <p className='text'>Some Information About Our Mission.</p>
+                    <p className='text'>The concept development is a web application that enables users from around the world to track planes that are shown on a real-time map. </p>
+                    <p  className='text'> Our Mission is to offer the users the ability to view information like plane details, weather, the estimation of planes' CO2 emissions, 
+                                          and suggested new locations that aviation enthusiasts can explore. </p>
+                    <p  className='text'> Moreover, the users can even create an account to leave comments on the blog page. </p>
                 </div>
             </div>
         </div>
