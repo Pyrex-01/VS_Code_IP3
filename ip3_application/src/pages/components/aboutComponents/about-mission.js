87fabf6df08ae2c3de91f7c970c4652d648e1f5e
AretiZ <74565922+AZILA200@users.noreply.github.com>
2023-03-31 12:03:21 +0100

    Update about-mission.js

--- ip3_application/src/pages/components/aboutComponents/about-mission.js
+++ ip3_application/src/pages/components/aboutComponents/about-mission.js
@@ -3,19 +3,24 @@ import React from 'react';
 const AboutMission = () => {
     return (
 
-        <div class="row">
-            <div class="col-sm-2 "> </div>
-            <div class="col-sm-6 mt-4">
-                <h className="h_about">Our Mission</h>
-                <div class="border border border-dark ">
-                    <p className='text'>The concept development is a web application that enables users from around the world to track planes that are shown on a real-time map. </p>
-                    <p  className='text'> Our Mission is to offer the users the ability to view information like plane details, weather, the estimation of planes' CO2 emissions, 
+       <section>
+          <div class="container">
+             <div class="row ">
+              <b><h class="title-1 ">Our Mission</h> </b>
+                <div class="col-md-6 ">
+                <p >The concept development is a web application that enables users from around the world to track planes that are shown on a real-time map. </p>
+                <p > Our Mission is to offer the users the ability to view information like plane details, weather, the estimation of planes' CO2 emissions, 
                                           and suggested new locations that aviation enthusiasts can explore. </p>
-                    <p  className='text'> Finally, the users can comment on the blog page and exchange their experiences with others. </p>
+                <p> Finally, the users can comment on the blog page and exchange their experiences with others. </p>
+                     
                 </div>
+      
+                 <div class="col-md-6 ">
+                 <img src="" className="" alt="" />
+                 </div>
             </div>
-        </div>
-
+          </div>
+      </section>
     )
 }
 
