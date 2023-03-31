e9ef0f402fb50471081d9e382ed444aeabfd835d
AretiZ <74565922+AZILA200@users.noreply.github.com>
2023-03-31 12:02:57 +0100

    Update about-team.js

--- ip3_application/src/pages/components/aboutComponents/about-team.js
+++ ip3_application/src/pages/components/aboutComponents/about-team.js
@@ -3,22 +3,21 @@ import React from 'react';
 
 const AboutTeam = () => {
   return (
-    <div class="row">
-      <div class="col-sm-2 ">
-        
-      </div>
-      <div class="col-sm-6 mt-4">
-        <h className="h_about">Our Team</h>
-        <div class="border border border-dark ">
-          <p className='text'> Our Team consist of 6 people from the department of Software Development of Business and Computing.</p>
-          <p className='text'> We collaborated  all together to create a project idea for the Integrated Project 3 module </p>
-        </div>
-
-      </div>
-
-    </div>
-
-
+    <section>
+          <div class="container">
+             <div class="row ">
+              <b><h class="title-1 ">Our Team</h> </b>
+                <div class="col-md-6 ">
+                <p > Our Team consist of 6 people from the department of Software Development of Business and Computing.</p>
+                <p > We collaborated  all together to create a project idea for the Integrated Project 3 module </p>    
+                </div>
+      
+                 <div class="col-md-6 ">
+                 <img src="" className="" alt="" />
+                 </div>
+            </div>
+          </div>
+      </section>
 
   )
 }
