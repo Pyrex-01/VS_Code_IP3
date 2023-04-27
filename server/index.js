b749c7b514d4a39bd4fd3241fbc8bb6e0679d4ac
jakeShep2002 <jsheph201@caledonian.ac.uk>
2023-04-27 16:00:25 +0100

    New API changes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var axios = require('axios');
+var cors = require("cors");
+let base64 = require('base-64');
 
 const params = {access_key: '8af9e33892c7fa1e9325b0dc9fe8e569'}
 
@@ -25,115 +27,96 @@ module.exports = app;
 
 const PORT = process.env.PORT || 3001;
 
-/*
-	Arrays to hold data from API
-	api_URL calls aviationstack API
-	api_URL2 calls opensky API with data from flight in area: switzerland
-*/
+var username = "jsgrape2002";
+var password = "inT3l#47";
+var headers = new Headers();
 
-const fullPlaneData = [];
+headers.append('Authorisation', 'Basic ' + base64.encode(username + ':' + password));
+
 const fullPlaneData2 = [];
 
-const api_URL = 'http://api.aviationstack.com/v1/flights?access_key=8af9e33892c7fa1e9325b0dc9fe8e569';
-const api_URL3 = 'https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226';
-
 /*
-	functions to call API and get the response
-	calls methods generatePlane Data
-
-	getAPIData = aviationStack
-	getAPIData = opensky
+  method getAPIData(), makes api call and stores data as json
+  calls method to handle data generatePlaneData
+  1) aviationStack
+  2) all plane data opensky
+  3) plane data from bounding box
+  4) aircraft between times
+  5) all flights between a certain time
 */
+const api_URL = 'http://api.aviationstack.com/v1/flights?access_key=8af9e33892c7fa1e9325b0dc9fe8e569';
 
-async function getAPIData() {
-	const response = await fetch(api_URL);
-	const data = await response.json();
-	generatePlaneData(data);
-  }
+const api_URL2 = 'https://opensky-network.org/api/states/all?icao24=';
 
-  async function getAPIData2() {
-	const response = await fetch(api_URL3);
-	const data = await response.json();
-	console.log(data);
-	generatePlaneData2(data);
-  }
+const api_URL3 = 'https://opensky-network.org/api/states/all?';
 
-  /*
-  	Methods generatePlaneData goes through the api and gets key values
-	Stores keyValues and puts them into a variable
-	Variable is then pushed into arrays
-	Repeats until all data has been handled
+const api_URL4 = 'https://opensky-network.org/api/flights/aircraft?';
 
-	generatePlaneData = aviationstack
-	generatePlaneData2 = opensky
-  */
+var api_URL5  = 'https://opensky-network.org/api/flights/all?';
 
-  const generatePlaneData = (data) => {
-	data.data.map((data) => {
-	  var airportDepart = data.departure.airport;
-	  var airportArrive = data.arrival.airport;
-	  var airlineName = data.airline.name;
-	  var airlineIATA = data.airline.iata;
-	  var ailineICAO = data.airline.icao;
-	  var flightIATA = data.flight.iata;
-	  var flightICAO = data.flight.icao;
+async function getAPIData2() {
+  var unixTime = Math.floor(Date.now()/1000);
+  console.log(unixTime, unixTime-10);
   
-	  var planeData = {
-		'Departure Airport': airportDepart,
-		'Airline Arival Airport': airportArrive,
-		'Airline Name': airlineName,
-		'Airline iata': airlineIATA,
-		'Airline icao': ailineICAO,
-		'Flight iata': flightIATA,
-		'Flight icao': flightICAO
-	  }
+  var api = api_URL5 + "begin=" + (unixTime-3600).toString() + "&end=" + unixTime.toString();
+  console.log(api);
+
+  const response = await fetch(api);
+  const data = await response.json();
+
+  console.log(data);
+
+  data.map((data) => {
+    generatePlaneData2(data.icao24, data.lastSeen);
+  })
+  console.log(fullPlaneData2);
+}
+
+async function generatePlaneData2(icao24, lastSeen) {
+  var icao = icao24;
+  var time = lastSeen;
+
+  var api2 = api_URL3 + "time=" + time.toString() + "&icao24=" + icao;
+  const response = await fetch(api2);
+  const data2 = await response.json();
+
+    var icao = data2[0];
+    var callsign = data2[1];
+    var origin_country = data2[2];
+    var longitude = data2[5];
+    var latitude = data2[6];
+    var altitude = data2[13];
+    var onGround = data2[8];
+    var velocity = data2[9];
+    var vertical_rate = data2[11];
+    var time = data2[3];
+
+    var planeData = {
+      'icao': icao,
+      'Callsign': callsign,
+      'Origin_Country': origin_country,
+      'Longitude': longitude,
+      'Latitude': latitude,
+      'Altitude': altitude,
+      'Landed': onGround,
+      'Velocity': velocity,
+      'Vertical_Rate': vertical_rate,
+      'Time': time
+      //'distance travveled': calcDistance(icao, data[3])
+    }
+    //calcDistance(icao, data[3]);
+    console.log(planeData);
+    fullPlaneData2.push(planeData);
+}
+
+async function calcDistance(icao, time) {
   
-	  fullPlaneData.push(planeData);
-  
-	  console.log(planeData);
-	})
-	console.log(fullPlaneData)
-  }
+}
 
-  const generatePlaneData2 = (data) => {
-	data.states.map((data) => {
-	  var icao = data[0];
-	  var callsign = data[1];
-	  var origin_country = data[2];
-	  var longitude = data[5];
-	  var latitude = data[6];
-	  var altitude = data[13];
-	  var onGround = data[8];
-	  var velocity = data[9];
-	  var vertical_rate = data[11];
-  
-	  var planeData = {
-		'icao': icao,
-		'Callsign': callsign,
-		'Origin_Country': origin_country,
-		'Longitude': longitude,
-		'Latitude': latitude,
-		'Altitude': altitude,
-		'Landed': onGround,
-		'Velocity': velocity,
-		'Vertical_Rate': vertical_rate
-	  }
-	  fullPlaneData2.push(planeData);
-	})
-	console.log(fullPlaneData2);
-  }
-
-  /*
-  	Method calls for running API
-
-	getAPIData = aviationstack API
-	getAPIData2 = opensky API
-  */
-
-//getAPIData();
+//getApiData2() is for opensky
 getAPIData2();
-  
 
+module.exports = app;
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
